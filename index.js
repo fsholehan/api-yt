d3c32760a13648fef3b6a5e0195d39b0a2c47b7d
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-08-28 07:41:44 +0700

    update package

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const AutoComplete = require("youtube-autocomplete");
 const app = express();
 const ytch = require("yt-channel-info");
 const cors = require("cors");
+const HttpsProxyAgent = require("https-proxy-agent");
+
+// Remove 'user:pass@' if you don't need to authenticate to your proxy.
+const proxy = "http://111.111.111.111:8080";
+const agent = HttpsProxyAgent(proxy);
 
 const port = process.env.PORT || 8000;
 
